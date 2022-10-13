0e16cc130fad04940ac0044ce224fdf1cd02a0c8
Matheus <matheus_maschietto@yahoo.com.br>
2022-10-12 21:27:02 -0300

    Update app.py

--- FlaskApplications/Ex1/app.py
+++ FlaskApplications/Ex1/app.py
@@ -3,6 +3,15 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/')
+def homePage():
+    return "Paws Rescue Center 🐾"
+
+
+@app.route('/about')
+def aboutPage():
+    return "We are a non-profit organization working as an animal rescue. We aim to help you connect with the purrfect furbaby for you! The animals you find on our website are rescued and rehabilitated animals. Our mission is to promote the ideology 'adopt, don't hop'! "
+
 
 if __name__ == '__main__':
     app.run()
