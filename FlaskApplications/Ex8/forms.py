4f2ba60941d8de4ced47134ed8bbcf3474388b8d
Matheus Zauza Maschietto <matheus_maschietto@yahoo.com.br>
2022-11-17 15:05:03 -0300

    update

--- FlaskApplications/Ex8/forms.py
+++ FlaskApplications/Ex8/forms.py
@@ -1,17 +1,18 @@
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Email, EqualTo
-from flask_wtf import FlaskForm
 
-
-class SigupForm(FlaskForm):
-    full_name = StringField('Full Name', validators=[InputRequired()])
-    email = StringField('E-Mail', validators=[InputRequired(), Email(message="Insert valid email format")])
-    password = PasswordField('Password', validators=[InputRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password', message='Passwords must match')])
-    submit = SubmitField('Signup')
-    
+class SignUpForm(FlaskForm):
+    full_name = StringField('Full Name', validators = [InputRequired()])
+    email = StringField('Email',
+                        validators = [InputRequired(), Email()])
+    password = PasswordField('Password', validators = [InputRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators = [InputRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
 
 class LoginForm(FlaskForm):
-    email = StringField('E-Mail', validators=[InputRequired(), Email(message="Insert valid email format")])
-    password = PasswordField('Password', validators=[InputRequired()])
+    email = StringField('Email',
+                        validators = [InputRequired(), Email()])
+    password = PasswordField('Password', validators = [InputRequired()])
     submit = SubmitField('Login')
